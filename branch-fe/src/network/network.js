f5098387589ba78e33545943f93931b6b51d4f37
aycakiremitci <ayca.kiremitci.99@gmail.com>
2023-05-25 14:48:56 +0300

    Add employee test user

--- branch-fe/src/network/network.js
+++ branch-fe/src/network/network.js
@@ -1,15 +1,21 @@
 import axios from "axios";
 
-const test_user = {
+const owner_test_user = {
   id: 1,
   username: "owner",
   role: "owner"
+};
+
+const employee_test_user = {
+  id: 1,
+  username: "employee",
+  role: "employee"
 };
 
 export default axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:6868/api' || 'http://localhost:8080/api',
   headers: {
     "Content-type": "application/json",
-    "user": JSON.stringify(test_user)
+    "user": JSON.stringify(owner_test_user)
   }
 });
